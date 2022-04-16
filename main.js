00519d3e07a3338d6e7173be0eb8c1a93c76af3f
Maity-123 <95854502+Maity-123@users.noreply.github.com>
2022-04-16 14:56:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,106 +1,58 @@
 
-<canvas id="myCanvas" width="800" height="400"> </canvas>
-canvas = document.getElementById('myCanvas ');
- ctx = canvas.getContext("2d");
 
+img_width = 300;
+img_height = 100;
 
+var img_image;
 
- greencar_width = 100;
- greencar_height = 100;
- greencar_x = 5;
-  greencar_y= 225;
-
-background_image = "parkingLot.jpg";
-greencar_image = "car2.png";
-
-
+img_x = 100;
+img_y = 100;
 
 function add() {
-	background_imgTag = new Image();
-	background_imgTag.onload = uploadBackground; background_imgTag.src = background_image;
-	greencar_imgTag = new Image();
-	greencar_imgTag.onload = uploadgreencar; greencar_imgTag.src = greencar_image;
-	}
-
-	function uploadBackground() {
-		ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
-		}
-	
-
-
-
-function uploadgreencar() {
-	function uploadgreencar() {
-		ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
-
-
-	
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
 
+function uploadimg() {
 
-window.addEventListener("keydown", my_keydown);
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
 
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-		if(keyPressed == '38')
-		{
-			up();
-			console.log("up");
-		}
 	
-		if(keyPressed == '40')
-		{
-			down();
-			console.log("down");
-		}
-		
-		if(keyPressed == '37')
-		{
-			left();
-			console.log("left");
-		}
-	
-		if(keyPressed == '39')
-		{
-			right();
-			console.log("right");
-		}
-		
-		
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		//write a code to check the type of key pressed
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
 }
 
+function aplhabetkey()
 {
-	function up()
-	{
-	if(greencar_y >=0)
-	greencar_y greencar y = 10;
-	console.log("When up arrow is pressed, x= " + greencar_x + uploadBackground(); uploadgreencar(); +greencar_y);
-	}
+	//upload respective image with the message. 
 
-
-	function down()
-{
-if(greencar_y >=0)
-greencar_y greencar y = -10;
-console.log("When down arrow is pressed, x= " + greencar_x + uploadBackground(); uploadgreencar(); +greencar_y);
 }
-
-
-
-	function left()
-	{
-	if(greencar_x >=0)
-	greencar_x greencarx_ x = 10;
-	console.log("When left arrow is pressed, x= " + greencar_x + uploadBackground(); uploadgreencar(); +greencar_y);
-	}
-		
-
-	function right()
-	{
-	if(greencar_x >=0)
-	greencar_x greencar x = -10;
-	console.log("When right arrow is pressed, x= " + greencar_x + uploadBackground(); uploadgreencar(); +greencar_y);
-	}
-	}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
